38f5b5b0f55957eb1be57a78959b305591976187
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2021-11-19 11:41:56 +0100

    create custom error page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout"
+
+export default function Custom404() {
+    return (
+    <Layout>
+        <h2>404 - Page Not Found</h2>
+        <p>Heya fellow this page does not exist, please click on the link hereunder to get back to the homepage</p>
+    </Layout>
+        )
+}
